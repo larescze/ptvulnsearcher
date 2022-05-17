8e9682d6299392fedc6bf942c770ea78ca3e00c7
Willi Lazarov <lazarov.w@gmail.com>
2022-05-17 20:57:29 +0200

    Update search function

--- ptvulnsearcher.py
+++ ptvulnsearcher.py
@@ -1,26 +1,30 @@
-import sys
-import getopt
 import requests
 import json
 import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-s", "--search", help="Search keywords (e.g. Apache v2.2)")
+parser.add_argument(
+    "-cve", "--cve", help="Search specific CVE")
+
 
 def search_cve(search_string, search_cve):
     api_url = "https://as.penterep.com:8443/api/v1/cve/search"
     parameters = {"search": search_string, "cve": search_cve}
     response = requests.get(api_url, params=parameters)
     response_json = response.json()
-    
+
     return json.dumps(response_json, indent=2)
 
+
 def search(args):
-    if(args.search):
+    if(args.search or args.cve):
         print(search_cve(args.search, args.cve))
+    else:
+        parser.print_help()
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--search")
-    parser.add_argument("-cve", "--cve")
-
     args = parser.parse_args()
-
     search(args)
