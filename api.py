92d9c9a6cb0355fb5772226216f468f91de6d76c
xBob45 <xmorav45@vutbr.cz>
2022-11-18 13:34:13 +0100

    model, resources . .

--- api.py
+++ api.py
@@ -1,19 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
-from marshmallow import Schema, fields
+
 
 
 #DB connection
 app = Flask(__name__)
+api = Api(app) #Wrap our app in an API 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost/postgres"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
 database = SQLAlchemy(app)
 
-
-#Creating model
-
-class cveTable(database.Model): #Receipe class in the video
+#Models
+class CveTableModel(database.Model): #Receipe class in the video
     id = database.Column(database.Integer(),primary_key=True)
     cve_id = database.Column(database.String(17), nullable = False)
     cwe_id = database.Column(database.String(15), nullable = True)
@@ -21,42 +20,42 @@ class cveTable(database.Model): #Receipe class in the video
     cvss_score = database.Column(database.Float, nullable = True)
     description = database.Column(database.Text, nullable = True)
 
+    def __repr__(self):
+        return f"Record(id = {self.id}, cve_id = {self.cve_id}, cwe_id = {self.cwe_id})"
 
+"""
 class vendorTable(database.Model):
     product_id = database.Column(database.Integer(),primary_key=True)
     cve_id = database.Column(database.String(17), nullable = False)
     vendor = database.Column(database.Text, nullable = True)
     product_type = database.Column(database.String(11), nullable = True)
     product_name = database.Column(database.Text, nullable = True)
-    version = database.Column(database.String(8), nullable = True)
+    version = database.Column(database.String(8), nullable = True)"""
 
-@classmethod
-def get_record_by_cve_id(cls,cve_id):
-    """Method that looks for and object and if it doesn't exit 404 is returned"""
-    return cls.query.get_or_404(cve_id)
+#Serilization
+resources_fields = {
+    "id":fields.Integer,
+    "cve_id":fields.String,
+    "cwe_id":fields.String,
+    "cvss_vector":fields.String,
+    "cvss_score":fields.Float,
+    "description":fields.String
 
-def commit(self):
-    database.session.commit()
+}
 
-
-#Creating serializer with 'marshmallow'
-class RecordsSchema(Schema):
-    #Specifieng the fields for the schema
-    id = fields.Integer()
-    cve_id = fields.String()
-    cwe_id = fields.String()
-    cvss_vector  =fields.String()
-    cvss_score = fields.Float()
-    description = fields.String()
-    #CONTINUE WITH FIELDS OF 'vendor' TABLE
+#Resources
+class resources(Resource):
+    @marshal_with(resources_fields)#Serilize the returned object according to 'resource_fields'
+    def get(self, cve_id):
+        result = CveTableModel.query.get(cve_id = CveTableModel.cve_id)
+        return result
 
 
 
-@app.route("/cve/<String:cve_id>", method=["GET"])
-def GetRecordBy_cve_id(cve_id):
-    pass
+#Registering resources
+api.add_resource(resources, "/<String:cve_id>")
 
 
-    
 
-app.run()
+
+app.run(debug=True)
