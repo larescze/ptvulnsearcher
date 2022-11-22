ede5796c12633723fde52eac9d0b7ffbf1706f54
xBob45 <xmorav45@vutbr.cz>
2022-11-22 08:16:02 +0100

    get record by cve_id -> WORKS!

--- api.py
+++ api.py
@@ -14,20 +14,21 @@ database = SQLAlchemy(app)
 
 #Models
 class cve(database.Model): #Receipe class in the video
-    id = database.Column(database.Integer(), primary_key=True)
-    cve_id = database.Column(database.String(17), nullable = False)
+    #id = database.Column(database.Integer(), primary_key=True)
+    cve_id = database.Column(database.String(17), primary_key = True)
     cwe_id = database.Column(database.String(15), nullable = True)
     cvss_vector = database.Column(database.String(40), nullable = True)
     cvss_score = database.Column(database.Float, nullable = True)
     description = database.Column(database.Text, nullable = True)
 
     def __repr__(self):
-        return f"resources(id = {self.id}, cve_id = {self.cve_id}, cwe_id = {self.cwe_id}, cvss_vector={self.cvss_vector}, cvss_score={self.cvss_score}.description={self.description})"
+        #return f"resources(id = {self.id}, cve_id = {self.cve_id}, cwe_id = {self.cwe_id}, cvss_vector={self.cvss_vector}, cvss_score={self.cvss_score}.description={self.description})"
+        return f"resources(cve_id = {self.cve_id}, cwe_id = {self.cwe_id}, cvss_vector={self.cvss_vector}, cvss_score={self.cvss_score}.description={self.description})"
 
 
 #Serilization
 resources_fields = {
-    "id":fields.Integer,
+    #"id":fields.Integer,
     "cve_id":fields.String,
     "cwe_id":fields.String,
     "cvss_vector":fields.String,
@@ -41,7 +42,7 @@ class resources(Resource):
     def get(self, cve_id):
         result = cve.query.get(cve_id)
         if result:
-            return result.info()
+            return result
         else:
             abort(404)
 
