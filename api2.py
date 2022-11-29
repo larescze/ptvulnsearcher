0680a794e08c2accb4f41415ed343d091b26d252
xBob45 <xmorav45@vutbr.cz>
2022-11-29 17:43:38 +0100

    search based on CVE-####-#### -> WORKS

--- api2.py
+++ api2.py
@@ -4,13 +4,16 @@ from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import relationship, Session
 from sqlalchemy import create_engine
 from sqlalchemy import select
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, fields, marshal_with
+import json
 
 
 #PostgreSQL connection
 
 #Isolation level = the ability of a database to allow a transaction to execute as if there are no other concurently running transactions. The goal is to prevent reads and writes of temporary/aborted or othewise incorrected data written by concurrent transaction
 #Create an engine = creates new database connection
-
+app = Flask(__name__)
 engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost/postgres", isolation_level = "AUTOCOMMIT")
 
 
@@ -60,12 +63,20 @@ class Vendor(Base):
     def __repr__(self) -> str: # "-> str" means that function returns a String type
         return f"Vendor(product_id = {self.product_id}, cveid = {self.cveid}, cvce_id={self.cve_id}, vendor={self.vendor},product_type={self.product_type}, product_name = {self.product_name}, version = {self.version})"
   
+
+    
+@app.route("/api/<string:cve_id>")
+def query_based_on_cve_id(cve_id):
+    with Session(engine) as session:
+
+        statement = select(Cve.cve_id, Cve.cwe_id, Cve.cvss_vector,Cve.cvss_score, Cve.description, Vendor.vendor, Vendor.product_type, Vendor.product_name, Vendor.version).join(Cve.vendors).where(Cve.cve_id == cve_id)
+        for row in session.execute(statement):
+            return(json.dumps(dict(row), sort_keys=True, indent=4, separators=(",\n",": ")))
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
     
 
-
-
-
-with Session(engine) as session:
-    results = session.query(Cve, Vendor).join("vendors").filter_by(cve_id = "CVE-1999-0001")
-
-print(results)
